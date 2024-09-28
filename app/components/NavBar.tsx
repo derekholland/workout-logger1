df684e6ec2dd6b90d5a0c90a13c31a391c5359e7
Derek Holland <derekhollandwebdev@gmail.com>
2024-09-28 12:01:22 -0400

    added margin to navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const Navbar = () => {
 	return (
-		<nav className='bg-gray-800 p-4'>
+		<nav className='bg-gray-800 p-4 mb-10'>
 			<div className='container mx-auto flex justify-between items-center'>
 				<div className='text-white text-2xl font-bold'>
 					<Link href='/'>Workout Logger</Link>
