5c5f50b2713048c2bd2e4c73b0195a5440a35af8
Derek Holland <derekhollandwebdev@gmail.com>
2024-10-03 13:38:11 -0400

    disable caching

--- app/api/get-workouts/route.ts
+++ app/api/get-workouts/route.ts
@@ -1,5 +1,30 @@
+// import { NextResponse } from 'next/server';
+// import { prisma } from '../../lib/prisma'; // Adjust this import path if necessary
+
+// export async function GET() {
+// 	try {
+// 		const workouts = await prisma.workout.findMany({
+// 			include: {
+// 				exercises: {
+// 					include: {
+// 						sets: true,
+// 					},
+// 				},
+// 			},
+// 		});
+// 		return NextResponse.json(workouts);
+// 	} catch (error) {
+// 		console.error('Error fetching workouts:', error);
+// 		return NextResponse.json(
+// 			{ error: 'Failed to fetch workouts' },
+// 			{ status: 500 },
+// 		);
+// 	}
+// }
+
+// /api/get-workouts/route.ts
 import { NextResponse } from 'next/server';
-import { prisma } from '../../lib/prisma'; // Adjust this import path if necessary
+import { prisma } from '../../lib/prisma';
 
 export async function GET() {
 	try {
@@ -12,7 +37,10 @@ export async function GET() {
 				},
 			},
 		});
-		return NextResponse.json(workouts);
+
+		const response = NextResponse.json(workouts);
+		response.headers.set('Cache-Control', 'no-store'); // Disable caching
+		return response;
 	} catch (error) {
 		console.error('Error fetching workouts:', error);
 		return NextResponse.json(
